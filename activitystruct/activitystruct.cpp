6bad49bece125188df32efc4ab24ad31dd257405
SyandyArda <syandi.2901@gmail.com>
2024-04-01 19:06:07 +0700

    project 1 selesai

--- activitystruct/activitystruct.cpp
+++ activitystruct/activitystruct.cpp
@@ -12,7 +12,18 @@ int main(){
     mahasiswa mhs;
     cout << "nomor Mahasiswa : ";
     cin >> mhs.nim;
-    
+    cout << "Nama Mahasiswa :";
+    cin >> mhs.nama;
+    cout << "alamat mahasiswa : ";
+    cin >> mhs.alamat;
+    cout << "Umur mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "\n Nim :" << mhs.nim;
+    cout << "\n Nama : " << mhs.nama;
+    cout << "\n alamat : " << mhs.alamat;
+    cout << "\n Umur : " << mhs.umur;
 }
 
 
